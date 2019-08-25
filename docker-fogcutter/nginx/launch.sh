c50c388d2fc68350d0767c577456afe57a8dc1c2
Paul Walko <paul@walko.org>
2019-08-25 17:06:13 -0400

    remove jail

--- docker-fogcutter/nginx/launch.sh
+++ docker-fogcutter/nginx/launch.sh
@@ -16,7 +16,6 @@ docker run \
     --volume $PWD/config:/config:rw \
     --volume $PWD/nginx.conf:/config/nginx/nginx.conf:ro \
     --volume $PWD/site-confs:/config/nginx/site-confs:ro \
-    --volume $PWD/jail.local:/config/jail2ban/jail.local:ro \
     --publish 10.42.0.203:80:80 \
     --publish 10.42.0.203:443:443 \
     --publish [2601:5c0:c100:6e65:96c6:91ff:feab:69e3]:80:80 \
