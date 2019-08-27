2bbb9253ddadb92215e6b46f2bf1dbdb3f988c2d
Paul Walko <paul@walko.org>
2019-08-26 21:25:28 -0400

    explicitly allow playlists & podcasts to be writable

--- docker-fogcutter/airsonic/launch.sh
+++ docker-fogcutter/airsonic/launch.sh
@@ -11,8 +11,8 @@ docker run \
     --env TZ=US/Easter \
     --volume $PWD/config:/config \
     --volume /bigdata/media/music:/media/music:ro \
-    --volume /bigdata/media/playlists:/media/playlists \
-    --volume /bigdata/media/podcasts:/media/podcasts:ro \
+    --volume /bigdata/media/playlists:/media/playlists:rw \
+    --volume /bigdata/media/podcasts:/media/podcasts:rw \
     --volume /media-vtluug:/media/media-vtluug:ro \
     --network pew-net \
     linuxserver/airsonic:latest
