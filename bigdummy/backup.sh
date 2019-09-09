d385bc6aa186e47c569b3f16fd3b6e818f385a53
Paul Walko <paul@seaturtle.pw>
2019-09-08 20:19:15 -0400

    backup bigdummy

--- bigdummy/backup.sh
+++ bigdummy/backup.sh
@@ -8,6 +8,14 @@ ARCHIVE_PATH=/bigdata/archive
 SERVICE_PATH=/home/paul/scripts/bigdummy/docker
 BACKUP_PATH=/bigdata/backups
 
+# Local backups
 if [ ! -f "$BACKUP_PATH"/linuxjournal.tar.gz ]; then
     tar czf "$BACKUP_PATH"/linuxjournal.tar.gz -C "$ARCHIVE_PATH" ./linuxjournal
 fi
+
+
+# Remote backups
+## Media
+rsync -avz -e "ssh -i /home/paul/.ssh/id_rsa_fast" --progress /bigdata/media/music/ pew-media@dirtycow.vtluug.org:/nfs/cistern/share/media/music/pew-media/
+## Misc
+rsync -avz -e "ssh -i /home/paul/.ssh/id_rsa_fast" --progress /bigdata/backups/ pew-media@dirtycow.vtluug.org:/nfs/cistern/home/pew-media/PEW-BACKUPS
