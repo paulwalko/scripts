e2cb1d9fb573168d3637b13c151248d54196b9a0
Paul Walko <paul@walko.org>
2019-09-08 20:04:12 -0400

    don't encrypt minecraft

--- fogcutter/backup.sh
+++ fogcutter/backup.sh
@@ -8,6 +8,6 @@ SERVICE_PATH=/home/paul/scripts/fogcutter/docker
 BACKUP_PATH=/bigdata/backups
 
 
-tar cz -C "$SERVICE_PATH"/minecraft ./minecraft-data | gpg -e -r $FINGERPRINT -o "$BACKUP_PATH"/minecraft-data-"$DATE".tar.gz.gpg
+tar czf "$BACKUP_PATH"/minecraft-data-"$DATE".tar.gz -C "$SERVICE_PATH"/minecraft ./minecraft-data
 
 tar cz -C "$SERVICE_PATH"/syncthing ./syncthing-sync | gpg -e -r $FINGERPRINT -o "$BACKUP_PATH"/syncthing-sync-"$DATE".tar.gz.gpg
